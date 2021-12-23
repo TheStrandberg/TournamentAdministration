92584baca296443654230d241ae27b9adccf3e20
RikhardHonkanen <75729661+RikhardHonkanen@users.noreply.github.com>
2021-12-23 09:23:13 +0100

    Dependency injection for AccessControl

--- TournamentAdministration/Startup.cs
+++ TournamentAdministration/Startup.cs
@@ -34,6 +34,8 @@ namespace TournamentAdministration
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<TournamentAdminContext>();
             services.AddRazorPages();
+
+            services.AddTransient<AccessControl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
