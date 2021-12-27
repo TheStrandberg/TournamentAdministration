4da6b0f08857ea4f89471069bd63092144d9625f
Dan Strandberg <DanStrandberg_@hotmail.com>
2021-12-27 12:36:52 +0100

    Lagt till sidorna för att lägga till venue och game. Inget funkar, men ändå...

--- TournamentAdministration/Pages/Games/Create.cshtml.cs
+++ TournamentAdministration/Pages/Games/Create.cshtml.cs
@@ -4,13 +4,53 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TournamentAdministration.Data;
+using TournamentAdmin.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace TournamentAdministration.Pages.Games
 {
     public class CreateModel : PageModel
     {
+        private readonly TournamentAdminContext database;
+        private readonly AccessControl accessControl;
+
+        public CreateModel(TournamentAdminContext database, AccessControl accessControl)
+        {
+            this.database = database;
+            this.accessControl = accessControl;
+        }
+
+        public Game Game { get; set; }
+
+        private void CreateEmptyGame()
+        {
+            Game = new Game
+            {
+
+            };
+        }
+
+        public async Task<IActionResult> OnPostAsync(Game game)
+        {
+
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
+
+            Game.Title = game.Title;
+
+            await database.Game.AddAsync(Game);
+            await database.SaveChangesAsync();
+            return RedirectToPage("./Details", new { game = game.ID });
+        }
+
         public void OnGet()
         {
+            CreateEmptyGame();
+            
         }
     }
 }
--- TournamentAdministration/Pages/Venues/Create.cshtml.cs
+++ TournamentAdministration/Pages/Venues/Create.cshtml.cs
@@ -4,13 +4,58 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TournamentAdministration.Data;
+using TournamentAdmin.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace TournamentAdministration.Pages.Venues
 {
     public class CreateModel : PageModel
     {
+        private readonly TournamentAdminContext database;
+        private readonly AccessControl accessControl;
+
+        public CreateModel(TournamentAdminContext database, AccessControl accessControl)
+        {
+            this.database = database;
+            this.accessControl = accessControl;
+        }
+
+        //public List<Venue> Venues { get; set; }
+        //public List<Game> Games { get; set; }
+        //public Tournament Tournament { get; set; }
+        public Venue Venue { get; private set; }
+
+        private void CreateEmptyVenue()
+        {
+            Venue = new Venue
+            {
+
+            };
+        }
+
+        public async Task<IActionResult> OnPostAsync(Venue venue)
+        {
+
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
+
+            Venue.VenueName = venue.VenueName;
+            Venue.Coordinate.Longitude = venue.Coordinate.Longitude;
+            venue.Coordinate.Latitude = venue.Coordinate.Latitude;
+
+
+            await database.Venue.AddAsync(Venue);
+            await database.SaveChangesAsync();
+            return RedirectToPage("./Details", new { venue = venue.ID });
+        }
+
         public void OnGet()
         {
+            CreateEmptyVenue();
         }
     }
 }
