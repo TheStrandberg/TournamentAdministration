5f3decd049062463e4b0ad699a41e22d4bcb603c
RikhardHonkanen <75729661+RikhardHonkanen@users.noreply.github.com>
2022-01-19 12:45:21 +0100

    Reworked API to single GET

--- TournamentAdministration/Controller/TournamentController.cs
+++ TournamentAdministration/Controller/TournamentController.cs
@@ -23,70 +23,101 @@ namespace TournamentAdministration.Controller
             this.database = database;
         }
 
+        //[HttpGet, AllowAnonymous]
+        //public async Task<ActionResult<IEnumerable<Tournament>>> GetTournament()
+        //{
+        //    return await database.Tournament.Include(g => g.Game).Include(v => v.Venue).AsNoTracking().OrderBy(t => t.EventTime).ToListAsync();
+        //}
+
+        //[HttpGet("/api/games/{title}"), AllowAnonymous]
+        //public async Task<ActionResult<IEnumerable<Tournament>>> FilterByGames(string title)
+        //{
+        //    var games = await database.Tournament.Include(g => g.Game).Include(v => v.Venue).OrderBy(t => t.EventTime).Where(g => g.Game.Title.ToLower().Contains(title.ToLower())).ToListAsync();
+
+        //    if (games == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return games;
+        //}
+
+        //[HttpGet("/api/venues/{venue}"), AllowAnonymous]
+        //public async Task<ActionResult<IEnumerable<Tournament>>> FilterByCitites(string venue)
+        //{
+        //    var venues = await database.Tournament.Include(g => g.Game).Include(v => v.Venue).OrderBy(t => t.EventTime).Where(v => v.Venue.VenueName.ToLower().Contains(venue.ToLower())).ToListAsync();
+
+        //    if (venues == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return venues;
+        //}
+
+        //[HttpGet("/api/Tournaments/{tournamentName}"), AllowAnonymous]
+        //public async Task<ActionResult<IEnumerable<Tournament>>> FilterByTournamentName(string tournamentName)
+        //{
+        //    var countries = await database.Tournament.Include(g => g.Game).Include(v => v.Venue).OrderBy(t => t.EventTime).Where(t => t.TournamentName.ToLower().Contains(tournamentName.ToLower())).ToListAsync();
+
+        //    if (countries == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return countries;
+        //}
+
         [HttpGet, AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<Tournament>>> GetTournament()
+        public async Task<ActionResult<IEnumerable<Tournament>>> GetTournamentNear(string name, string game, string venue,
+            string country, string city, int? distance, double latitude, double longitude)
         {
-            return await database.Tournament.Include(g => g.Game).Include(v => v.Venue).AsNoTracking().OrderBy(t => t.EventTime).ToListAsync();
-        }
+            var tournaments = await database.Tournament.Include(g => g.Game).Include(v => v.Venue).AsNoTracking().OrderBy(t => t.EventTime).ToListAsync();
 
-        [HttpGet("/api/games/{title}"), AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<Tournament>>> FilterByGames(string title)
-        {
-            var games = await database.Tournament.Include(g => g.Game).Include(v => v.Venue).OrderBy(t => t.EventTime).Where(g => g.Game.Title.ToLower().Contains(title.ToLower())).ToListAsync();
-
-            if (games == null)
+            if (name != null)
             {
-                return NotFound();
+                tournaments = tournaments.Where(t => t.TournamentName.ToLower().Contains(name.ToLower())).ToList();
             }
 
-            return games;
-        }
-
-        [HttpGet("/api/venues/{venue}"), AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<Tournament>>> FilterByCitites(string venue)
-        {
-            var venues = await database.Tournament.Include(g => g.Game).Include(v => v.Venue).OrderBy(t => t.EventTime).Where(v => v.Venue.VenueName.ToLower().Contains(venue.ToLower())).ToListAsync();
-
-            if (venues == null)
+            if (game != null)
             {
-                return NotFound();
+                tournaments = tournaments.Where(t => t.Game.Title.ToLower().Contains(game.ToLower())).ToList();
             }
 
-            return venues;
-        }
-
-        [HttpGet("/api/Tournaments/{tournamentName}"), AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<Tournament>>> FilterByTournamentName(string tournamentName)
-        {
-            var countries = await database.Tournament.Include(g => g.Game).Include(v => v.Venue).OrderBy(t => t.EventTime).Where(t => t.TournamentName.ToLower().Contains(tournamentName.ToLower())).ToListAsync();
-
-            if (countries == null)
+            if (venue != null)
             {
-                return NotFound();
+                tournaments = tournaments.Where(t => t.Venue.VenueName.ToLower().Contains(venue.ToLower())).ToList();
             }
 
-            return countries;
-        }
-
-        [HttpGet("/api/Tournaments/near"), AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<Tournament>>> GetTournamentNear(int distance, double latitude, double longitude)
-        {
-            var tournaments = await database.Tournament.Include(t => t.Venue).Include(t => t.Game).OrderBy(t => t.EventTime).ToListAsync();
-            var tournamentsInRange = new List<Tournament>();
-            var userCoordinate = new GeoCoordinate(latitude, longitude);
-
-            foreach (var tournament in tournaments)
+            if (country != null)
             {
-                var venueCoordinate = new GeoCoordinate(tournament.Venue.Coordinate.Latitude, tournament.Venue.Coordinate.Longitude);
-                var actualDistance = userCoordinate.GetDistanceTo(venueCoordinate);
+                tournaments = tournaments.Where(t => t.Venue.Address.Country.ToLower().Contains(country.ToLower())).ToList();
+            }
 
-                if (actualDistance < distance * 1000)
+            if (city != null)
+            {
+                tournaments = tournaments.Where(t => t.Venue.Address.City.ToLower().Contains(city.ToLower())).ToList();
+            }
+
+            if (distance != null)
+            {
+                var tournamentsInRange = new List<Tournament>();
+                var userCoordinate = new GeoCoordinate(latitude, longitude);
+
+                foreach (var tournament in tournaments)
                 {
-                    tournamentsInRange.Add(tournament);
+                    var venueCoordinate = new GeoCoordinate(tournament.Venue.Coordinate.Latitude, tournament.Venue.Coordinate.Longitude);
+                    var actualDistance = userCoordinate.GetDistanceTo(venueCoordinate);
+
+                    if (actualDistance < distance * 1000)
+                    {
+                        tournamentsInRange.Add(tournament);
+                    }
                 }
+                tournaments = tournamentsInRange;
             }
 
-            return tournamentsInRange;
+            return tournaments;
         }
     }
 }
